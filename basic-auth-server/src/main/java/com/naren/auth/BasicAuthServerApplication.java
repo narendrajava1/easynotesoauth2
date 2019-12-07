d9600c8d22a4894ae0238c1f4cb4a994918a0380
narendrajava1 <narendrakolli08@gmail.com>
2019-12-07 13:00:11 +0530

    started

---
+++ basic-auth-server/src/main/java/com/naren/auth/BasicAuthServerApplication.java
@@ -0,0 +1,16 @@
+package com.naren.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicAuthServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BasicAuthServerApplication.class, args);
+
+
+
+
+	}
+
+}
